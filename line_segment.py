536f2d1575b99335259a3eb58cd1a9b249052c89
frugiam <frugiam@oregonstate.edu>
2024-03-06 19:32:17 -0800

    line_segment.py

--- line_segment.py
+++ line_segment.py
@@ -45,7 +45,21 @@ class LineSegment:
         x2, y2 = self._endpoint_2.get_x_coord(), self._endpoint_2.get_y_coord()
 
         if x2 - x1 == 0:
-            # Avoid division by zero for vertical lines
+            "'Avoid division by zero for vertical lines'"
             return float('inf')
         else:
             return (y2 - y1) / (x2 - x1)
+
+    "'Example usage'"
+    point_1 = Point(7, 4)
+    point_2 = Point(-6, 18)
+    print(point_1.distance_to(point_2))
+
+    line_seg_1 = LineSegment(point_1, point_2)
+    print(line_seg_1.length())
+    print(line_seg_1.slope())
+
+    point_3 = Point(-2, 2)
+    point_4 = Point(24, 12)
+    line_seg_2 = LineSegment(point_3, point_4)
+    print(line_seg_1.is_parallel_to(line_seg_2))
